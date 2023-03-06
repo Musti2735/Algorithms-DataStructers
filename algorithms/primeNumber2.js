a7c2639e8593a39fa16e27a67e02eacb6e06bbfc
Mustafa Turk <av.mustafaturk@hotmail.com>
2023-03-06 15:33:50 +0300

    primeNumber

---
+++ algorithms/primeNumber2.js
@@ -0,0 +1,18 @@
+function primeNumber(n) {
+    if (n < 2) {
+        return false
+    }
+    for (let i = 2; i < Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return true
+
+}
+
+console.log(primeNumber(6)) // false
+console.log(primeNumber(11)) // true
+console.log(primeNumber(17)) // true
+
+//bigO(sqrt(n))
